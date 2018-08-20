bbdae1354d542475dcb51e7bdd510faac4585139
Andreas Dröscher <github@anticat.ch>
2018-08-20 21:37:32 +0200

    change: reworked legic receive for bumped xcorr rate
    The updated xcorr mode reports a sample every 4.7 us (instead
    of 18.9us). This patch adjusts rx_bit for the new rate and
    indirectly improve the range.

--- armsrc/legicrf.c
+++ armsrc/legicrf.c
@@ -71,40 +71,46 @@ static inline uint8_t rx_byte_from_fpga() {
 // Demodulation
 //-----------------------------------------------------------------------------
 
-// Returns am aproximated power measurement
+// Returns a demedulated bit
 //
-// The FPGA running on the xcorrelation kernel samples the subcarrier at ~3 MHz.
-// The kernel was initialy designed to receive BSPK/2-PSK. Hance, it reports an
-// I/Q pair every 18.9us (8 bits i and 8 bits q).
+// The FPGA running xcorrelation samples the subcarrier at ~13.56 MHz. The mode
+// was initialy designed to receive BSPK/2-PSK. Hance, it reports an I/Q pair
+// every 4.7us (8 bits i and 8 bits q).
 //
 // The subcarrier amplitude can be calculated using Pythagoras sqrt(i^2 + q^2).
 // To reduce CPU time the amplitude is approximated by using linear functions:
 //   am = MAX(ABS(i),ABS(q)) + 1/2*MIN(ABS(i),ABSq))
 //
 // Note: The SSC receiver is never synchronized the calculation my be performed
-// on a i/q pair from two subsequent correlations, but does not matter.
-static inline int32_t sample_power() {
-  int32_t q = (int8_t)rx_byte_from_fpga(); q = ABS(q);
-  int32_t i = (int8_t)rx_byte_from_fpga(); i = ABS(i);
-
-  return MAX(i, q) + (MIN(i, q) >> 1);
-}
-
-// Returns a demedulated bit
+// on a I/Q pair from two subsequent correlations, but does not matter.
 //
-// An aproximated power measurement is available every 18.9us. The bit time
-// is 100us. The code samples 5 times and uses the last (most stable) sample.
+// The bit time is 99.1us (21 I/Q pairs). The receiver skips the first 5 samples
+// and averages the next (most stable) 8 samples. The final 8 samples are dropped
+// also.
 //
-// Note: The demodulator would be drifting (18.9us * 5 != 100us), rx_frame
-// has a delay loop that aligns rx_bit calls to the TAG tx timeslots.
+// The demedulated should be alligned to the bit periode by the caller. This is
+// done in rx_bit_as_reader and rx_ack_as_reader.
 static inline bool rx_bit() {
-  int32_t power;
+  int32_t cq = 0;
+  int32_t ci = 0;
 
+  // skip first 5 I/Q pairs
   for(size_t i = 0; i<5; ++i) {
-    power = sample_power();
+    (int8_t)rx_byte_from_fpga();
+    (int8_t)rx_byte_from_fpga();
   }
 
-  return (power > INPUT_THRESHOLD);
+  // sample next 8 I/Q pairs
+  for(size_t i = 0; i<8; ++i) {
+    cq += (int8_t)rx_byte_from_fpga();
+    ci += (int8_t)rx_byte_from_fpga();
+  }
+
+  // calculate power
+  int32_t power = (MAX(ABS(ci), ABS(cq)) + (MIN(ABS(ci), ABS(cq)) >> 1));
+
+  // compare average (power / 8) to threshold
+  return ((power >> 3) > INPUT_THRESHOLD);
 }
 
 //-----------------------------------------------------------------------------
